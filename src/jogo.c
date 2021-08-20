0aeddc52b20dc5822a157cdcbdfa10c86bf40510
MarcusXavierr <marcusxavierr123@gmail.com>
2021-08-19 23:22:20 -0300

    adicionei funções para comer o adversário

--- src/jogo.c
+++ src/jogo.c
@@ -2,6 +2,7 @@
 #include "jogo.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void desenharTabuleiro(Jogador player1, Jogador player2)
 {
@@ -45,7 +46,7 @@ void desenharTabuleiro(Jogador player1, Jogador player2)
     printf("\n");
 }
 
-void jogada(Jogador *jogador)
+void jogada(Jogador *jogador, Jogador *adversario)
 {
     char message[50], input[50];
     char Cl1, Cl2;
@@ -53,7 +54,7 @@ void jogada(Jogador *jogador)
     int pieceExists = 0;
     char tipo;
     sprintf(message, "%s, é a sua vez: ", jogador->nome);
-    lerJogada(message, input, &c1, &c2, &l1, &l2, &linha, &tipo, jogador);
+    lerJogada(message, input, &c1, &c2, &l1, &l2, &linha, &tipo, jogador, adversario);
     // TODO: Verificar se o user está indo para trás, ou para frente
     jogador->pecas[linha][0] = l2;
     jogador->pecas[linha][1] = c2;
@@ -73,10 +74,12 @@ int switchChar(char c) {
 
 }
 
-void lerJogada(char *message, char *input, int *c1, int *c2, int *l1, int *l2, int *linha, char *tipo, Jogador *jogador)
+void lerJogada(char *message, char *input, int *c1, int *c2, int *l1, int *l2, int *linha, char *tipo, Jogador *jogador, Jogador *adversario)
 {   
     int is_valid = 0, qtd_input;
     char Cl1, Cl2;
+    int tmp;
+    char adversario_type[1];
     getInputFromUser(message, input);
     qtd_input = sscanf(input,"%c%d %c%d", &Cl1, c1, &Cl2, c2);
     while(qtd_input != 4){
@@ -89,22 +92,64 @@ void lerJogada(char *message, char *input, int *c1, int *c2, int *l1, int *l2, i
     *l2 = switchChar(Cl2);
     *c1 = *c1 - 1; *c2 = *c2 - 1;
     if(*l1 < 0 || *l2 < 0 || *c1 < 0 || *c2 < 0 || *c1 > 7 || *c2 > 7) goto inputError;
-    int exists = verifyIfPieceExists(jogador, *l1, *c1, linha, tipo);
-    if(!exists) goto inputError;
-    // verifyIfDirectionIsRight()
+    int exists = verifyIfPieceExists(*jogador, *l1, *c1, linha, tipo);
+    int espacoOcupado = verifyIfPieceExists(*adversario, *l2, *c2, &tmp, adversario_type);
+    if(!exists || espacoOcupado) goto inputError;
+    int distancia = calcularDistanciaMovimento(*l1, *c1, *l2, *c2);
+
+    // TODO: ARRUMAR ESSA MASSAROCA DEPOIS
+    if(*tipo == 'x' || *tipo == 'o'){
+        int ladoCerto = verifyIfDirectionIsRight(*l1, *l2, *tipo);
+        if(!ladoCerto) goto inputError;
+        if(distancia > 2) goto inputError;
+        if(distancia > 1){
+            int comeu = verificarSeComeu(jogador, adversario, *l1, *c1, *l2, *c2);
+            if(!comeu) goto inputError;
+        }
+    }
     
 }
 
-int verifyIfDirectionIsRight(int c1, int l1, int c2, int l2, char type)
+void removerPeca(Jogador *jogador, int linha){
+    
+    for(; linha < (jogador->qtd_pecas - 1); linha++ ){
+        jogador->pecas[linha][0] = jogador->pecas[linha + 1][0];
+        jogador->pecas[linha][1] = jogador->pecas[linha + 1][1];
+        jogador->pecas[linha][2] = jogador->pecas[linha + 1][2];
+    }
+    free(jogador->pecas[linha]);
+    jogador->qtd_pecas--;
+}
+int verificarSeComeu(Jogador *jogador, Jogador *adversario, int l1, int c1, int l2, int c2){
+    int xM, yM, localizacao_peca;
+    char tipo;
+    xM = (c1 + c2) / 2;
+    yM = (l1 + l2) / 2;
+    int exists = verifyIfPieceExists(*adversario,yM, xM, &localizacao_peca, &tipo);
+    if(exists){
+        removerPeca(adversario, localizacao_peca);
+        return 1;
+    }
+    return 0;
+}   
+int calcularDistanciaMovimento(int l1, int c1, int l2, int c2){
+    int distancia, x, y;
+    x = pow((c1 - c2), 2); 
+    y = pow((l1 - l2), 2); 
+    distancia = sqrt((x + y));
+    return distancia;
+}
+
+int verifyIfDirectionIsRight(int l1, int l2, char type)
 {
     if(type == 'o')
     {
-        if(c1 > c2) return 1;
+        if(l1 > l2) return 1;
         else return 0;
     }
     if(type == 'x')
     {
-        if(c1 < c2) return 1;
+        if(l1 < l2) return 1;
         else return 0;
     }
 
@@ -114,15 +159,24 @@ int verifyIfDirectionIsRight(int c1, int l1, int c2, int l2, char type)
     }
 }
 
-int verifyIfPieceExists(Jogador *jogador, int l1, int c1, int *result, char *type){
-    for(int linha = 0; linha < jogador->qtd_pecas; linha++){
-        int linhaIgual = jogador->pecas[linha][0] == l1;
-        int colunaIgual = jogador->pecas[linha][1] == c1;
+int verifyIfPieceExists(Jogador jogador, int l1, int c1, int *result, char *type){
+    for(int linha = 0; linha < jogador.qtd_pecas; linha++){
+        int linhaIgual = jogador.pecas[linha][0] == l1;
+        int colunaIgual = jogador.pecas[linha][1] == c1;
         if(linhaIgual && colunaIgual){
-            *type = jogador->pecas[linha][2];
+            *type = jogador.pecas[linha][2];
             *result = linha;
             return 1;
         }
     }
     return 0;
+}
+
+int verifyIfUserWannaSave(char *input){
+    int result = strcmp(input, "salvar");
+    if(result == 0){
+        return 1;
+    }else{
+        return 0;
+    }
 }
--- src/jogo.h
+++ src/jogo.h
@@ -1,9 +1,16 @@
 #ifndef JOGO_H
 #define JOGO_H
 #include "jogador.h"
+#include "math.h"
 void desenharTabuleiro(Jogador player1, Jogador player2);
-void jogada(Jogador *jogador);
-void lerJogada(char *message, char *input, int *c1, int *c2, int *l1, int *l2, int *linha, char *tipo, Jogador *jogador);
+int calcularDistanciaMovimento(int l1, int c1, int l2, int c2);
+void removerPeca(Jogador *jogador, int linha);
+int verificarSeComeu(Jogador *jogador, Jogador *adversario, int l1, int c1, int l2, int c2);
+void jogada(Jogador *jogador, Jogador *adversario);
+
+void lerJogada(char *message, char *input, int *c1, int *c2, int *l1, int *l2, int *linha, char *tipo, Jogador *jogador, Jogador *adversario);
 int switchChar(char c);
-int verifyIfPieceExists(Jogador *jogador, int l1, int c1, int *result, char *type);
+int verifyIfDirectionIsRight(int l1, int l2, char type);
+int verifyIfPieceExists(Jogador jogador, int l1, int c1, int *result, char *type);
+int verifyIfUserWannaSave(char *input);
 #endif
