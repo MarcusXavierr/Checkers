c9d80cc064d5f082b509eb73c28fcde067c5ee40
Marcus Xavier <59923581+MarcusXavierr@users.noreply.github.com>
2021-08-23 15:55:22 -0300

    diminuí o tempo de sleep

--- src/jogo.c
+++ src/jogo.c
@@ -59,7 +59,7 @@ void jogada(Jogador *jogador, Jogador *adversario, int *turno, char *nome_arquiv
     int is_bot = strcmp(jogador->nome, "myself");
     if(is_bot == 0){
         jogarComBot( &c1, &c2, &l1, &l2, &linha, tipo, jogador, adversario, *turno, &pecas_comidas);
-        sleep(1);
+        usleep(650000);
     }else{
         lerJogada(message, input, &c1, &c2, &l1, &l2, &linha, tipo, jogador, adversario, *turno, &pecas_comidas, nome_arquivo);
     }
