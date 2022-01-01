62648d16f53eaeddefdff091914bdf29363ad70e
MarcusXavierr <marcusxavierr123@gmail.com>
2022-01-01 17:54:20 -0300

    fix minor bugs

--- src/menu.h
+++ src/menu.h
@@ -4,11 +4,14 @@
 
 #ifndef MENU_H
 #define MENU_H
-// #include "jogador.h"
+#include <stdio.h>
+
+#include "jogador.h"
+
 void printMenu(Jogador *jogador1, Jogador *jogador2, int *rodada, char *nome_arquivo);
 void getMenuInput(int *choice, const char *message);
 void carregarJogo(Jogador *jogador1, Jogador *jogador2, char *nome_arquivo, int *rodada);
-void readNameFromFile(Jogador *jogador, FILE *f, char* buffer, int bufferLength);
+void readNameFromFile(Jogador *jogador, FILE *f, char *buffer, int bufferLength);
 char *remove_white_spaces(char *str);
 
 #endif
